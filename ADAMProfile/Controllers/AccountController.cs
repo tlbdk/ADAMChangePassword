c78e6e125c16107f384a2dffc43333bad077186d
Troels Liebe Bentsen <troels@it-kartellet.dk>
2012-07-30 12:00:01 +0200

    Add support for old interface and port ADAMProfile to the new lib

--- ADAMProfile/Controllers/AccountController.cs
+++ ADAMProfile/Controllers/AccountController.cs
@@ -8,6 +8,7 @@ using System.Net;
 using System.Text;
 using ADAMProfile.Models;
 using System.Web.Configuration;
+using LDAPUtils;
 
 namespace ADAMProfile.Controllers
 {
@@ -50,7 +51,7 @@ namespace ADAMProfile.Controllers
             {
                 try
                 {
-                    if (setPassword(ADAMUrl, model.UserName, model.OldPassword, model.NewPassword))
+                    if (LDAPAccount.ChangePassword(ADAMUrl, model.UserName, model.OldPassword, model.NewPassword))
                     {
                         return RedirectToAction("ChangePasswordSuccess");
                     }
@@ -77,77 +78,5 @@ namespace ADAMProfile.Controllers
         {
             return View();
         }
-
-        static bool setPassword(string url, string user, string oldPassword, string newPassword)
-        {
-            var adamUri = new Uri(url);
-            var userDN = "CN=" + user + "," + adamUri.LocalPath.Substring(1);
-
-            // Create connection without SSL and other security
-            LdapConnection connection = new LdapConnection(adamUri.Host + ":" + adamUri.Port);
-            if (adamUri.Scheme == "ldap")
-            {
-                connection.SessionOptions.SecureSocketLayer = false;
-                connection.SessionOptions.Sealing = true;
-                connection.SessionOptions.Signing = false;
-            }
-            else if (adamUri.Scheme == "ldaps")
-            {
-                connection.SessionOptions.SecureSocketLayer = true;
-            }
-            else
-            {
-                throw new Exception("Unknown connection type:" + adamUri.Scheme);
-            }
-
-            // Basic bind with user and old password
-            NetworkCredential credential = new NetworkCredential(userDN, oldPassword);
-            connection.AuthType = AuthType.Basic;
-            try
-            {
-                connection.Bind(credential);
-            }
-            catch (LdapException ex)
-            {
-                // Invalid credentials
-                if (ex.ErrorCode == 49)
-                {
-                    return false;
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            // Create change password request
-            DirectoryAttributeModification deleteMod = new DirectoryAttributeModification();
-            deleteMod.Name = "unicodePwd";
-            deleteMod.Add(Encoding.Unicode.GetBytes("\"" + oldPassword + "\""));
-            deleteMod.Operation = DirectoryAttributeOperation.Delete;
-            DirectoryAttributeModification addMod = new DirectoryAttributeModification();
-            addMod.Name = "unicodePwd";
-            addMod.Add(Encoding.Unicode.GetBytes("\"" + newPassword + "\""));
-            addMod.Operation = DirectoryAttributeOperation.Add;
-            ModifyRequest request = new ModifyRequest(userDN, deleteMod, addMod);
-
-            try
-            {
-                DirectoryResponse response = connection.SendRequest(request);
-                return response.ResultCode == 0;
-            }
-            catch (Exception ex)
-            {
-                if (ex.Message == "The object does not exist." && adamUri.Scheme == "ldap")
-                {
-                    throw new Exception("User not allowed to change own password because of missing permission, set dsHeuristics to 0000000001001 on CN=Directory Service,CN=Windows NT,CN=Services,CN=Configuration,CN=...");
-                }
-                else
-                {
-                    throw;
-                }
-            }
-        }
-
     }
 }
--- LDAPUtils/Account.cs
+++ LDAPUtils/Account.cs
@@ -50,6 +50,14 @@ namespace LDAPUtils
             return connection;
         }
 
+        public static bool SetPassword(String url, string adminPassword, string userDN, string newPassword, bool dryRun = false)
+        {
+            var adamUri = new Uri(url);
+            var bindDN = adamUri.LocalPath.Substring(1);
+            var conn = LDAPAccount.LdapConnectBind(adamUri, bindDN, adminPassword);
+            return SetPassword(conn, userDN, newPassword, dryRun);
+        }
+
         public static bool SetPassword(LdapConnection connection, string userDN, string newPassword, bool dryRun = false)
         {
             DirectoryAttributeModification addMod = new DirectoryAttributeModification();
@@ -83,6 +91,14 @@ namespace LDAPUtils
             }
         }
 
+        public static bool ChangePassword(String url, string user, string oldPassword, string newPassword, bool dryRun = false)
+        {
+            var adamUri = new Uri(url);
+            var bindDN = "CN=" + user + "," + adamUri.LocalPath.Substring(1);
+            var conn = LDAPAccount.LdapConnectBind(adamUri, bindDN, oldPassword);
+            return ChangePassword(conn, bindDN, oldPassword, newPassword, dryRun);
+        }
+
         public static bool ChangePassword(LdapConnection connection, string userDN, string oldPassword, string newPassword, bool dryRun = false)
         {
             // Create change password request
@@ -108,13 +124,14 @@ namespace LDAPUtils
                     return true;
                 }
             }
-            // TODO: Convert to DirectoryOperationException and use better match to give the dsHeuristics exception
+            
             catch (DirectoryOperationException ex)
             {
                 if (ex.Response.ErrorMessage.StartsWith("0000052D"))
                 {
                     throw new Exception("Unable to update the password. The value provided for the new password does not meet the length, complexity, or history requirements of the domain.");
                 }
+                // TODO: Convert to DirectoryOperationException and use better match to give the dsHeuristics exception
                 else if (ex.Message == "The object does not exist")
                 {
                     throw new Exception("User not allowed to change own password because of missing permission, set dsHeuristics to 0000000001001 on CN=Directory Service,CN=Windows NT,CN=Services,CN=Configuration,CN=...");
